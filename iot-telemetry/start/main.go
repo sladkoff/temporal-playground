6ba32f2aa38335a9785b89d5b07679f810f8bd43
Leonid Koftun <leonid.koftun@device-insight.com>
2024-04-26 13:52:06 +0200

    chore: generate input data

--- iot-telemetry/start/main.go
+++ iot-telemetry/start/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
 	app "iot-telemetry"
 	"iot-telemetry/types"
 	"log"
+	"math/rand"
 	"os"
 
 	"go.temporal.io/sdk/client"
@@ -24,33 +26,60 @@ func main() {
 	defer c.Close()
 
 	options := client.StartWorkflowOptions{
-		//ID:        "ingest-workflow",
 		TaskQueue: app.DeviceMessagesQueue,
 	}
 
-	input := types.Input{
-		DeviceID:    "123",
-		ReceiveTime: "2021-01-01T00:00:00Z",
-		Content:     args[0],
+	inputs := []types.Input{}
+
+	if len(args) == 0 {
+		inputs = generateInputs(uuid.New().String(), 10)
+	} else {
+		inputs = append(inputs, types.Input{
+			DeviceID:    "device-1",
+			ReceiveTime: "2021-01-01T00:00:00Z",
+			Content:     args[0],
+		})
 	}
 
-	// Start the Workflow
-	we, err := c.SignalWithStartWorkflow(context.Background(), input.DeviceID, "message-signal", input, options, app.IngestWorkflow)
-	if err != nil {
-		log.Fatalln("unable to complete Workflow", err)
+	// iterate over inputs
+	var we client.WorkflowRun
+	for _, input := range inputs {
+		// Start the Workflow
+		we, err = c.SignalWithStartWorkflow(context.Background(), fmt.Sprintf("ingest-%s", input.DeviceID), "message-signal", input, options, app.IngestWorkflow)
+		if err != nil {
+			log.Fatalln("unable to complete Workflow", err)
+		}
 	}
 
 	// Get the results
-	var greeting string
-	err = we.Get(context.Background(), &greeting)
+	average, err := c.QueryWorkflow(context.Background(), we.GetID(), we.GetRunID(), "average", nil)
 	if err != nil {
-		log.Fatalln("unable to get Workflow result", err)
+		return
 	}
 
-	printResults(greeting, we.GetID(), we.GetRunID())
+	var value float64
+	err = average.Get(&value)
+
+	if err != nil {
+		log.Fatalln("unable to get average", err)
+	}
+
+	printResults(value, we.GetID(), we.GetRunID())
 }
 
-func printResults(greeting string, workflowID, runID string) {
+func generateInputs(device string, count int) []types.Input {
+	var inputs []types.Input
+	for i := 0; i < count; i++ {
+		inputs = append(inputs, types.Input{
+			DeviceID:    device,
+			ReceiveTime: "2021-01-01T00:00:00Z",
+			Content:     fmt.Sprintf(`{"telemetry": %f, "errors": []}`, rand.Float64()),
+		})
+	}
+	return inputs
+}
+
+func printResults(average float64, workflowID, runID string) {
 	fmt.Printf("\nWorkflowID: %s RunID: %s\n", workflowID, runID)
-	fmt.Printf("\n%s\n\n", greeting)
+	fmt.Printf("\n%f\n\n", average)
 }
